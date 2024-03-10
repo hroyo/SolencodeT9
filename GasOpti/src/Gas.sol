727898883c8698acff8fb04d8d644c7a6b1cde0d
BigBangInfinity <chinesedragon2000@gmail.com>
2024-03-10 21:07:32 +0000

    session with Bojezyk"

--- GasOpti/src/Gas.sol
+++ GasOpti/src/Gas.sol
@@ -13,8 +13,7 @@ contract GasContract {
     uint256 immutable totalSupply;
     uint8 paymentCounter = 0;
     mapping(address => uint256) public balances;
-    // tradePercent is being used as a constant for setting the lenght of the status array in addHistory function which is not needed
-    //uint8 constant tradePercent = 12;
+
     address immutable contractOwner;
     mapping(address => Payment[]) payments;
     mapping(address => uint256) public whitelist;
@@ -29,7 +28,7 @@ contract GasContract {
     // defaultPayment is not being used can be removed.
     //PaymentType constant defaultPayment = PaymentType.Unknown;
 
-    History[] public paymentHistory; // when a payment was updated
+    // History[] paymentHistory; // when a payment was updated
 
     struct Payment {
         PaymentType paymentType;
@@ -41,23 +40,23 @@ contract GasContract {
         uint256 amount;
     }
 
-    struct History {
-        uint32 lastUpdate;
-        address updatedBy;
-        uint32 blockNumber;
-    }
+    // struct History {
+    //     uint32 lastUpdate;
+    //     address updatedBy;
+    //     uint32 blockNumber;
+    // }
     // uint8 wasLastOdd = 1;
     // mapping(address => uint8) public isOddWhitelistUser;
 
     struct ImportantStruct {
         uint256 amount;
-        uint8 valueA; // max 3 digits
-        uint8 bigValue;
-        uint8 valueB; // max 3 digits
+        // uint8 valueA; // max 3 digits
+        // uint8 bigValue;
+        // uint8 valueB; // max 3 digits
         bool paymentStatus;
-        address sender;
+        // address sender;
     }
-    mapping(address => ImportantStruct) public whiteListStruct;
+    mapping(address => ImportantStruct) whiteListStruct;
 
     event AddedToWhitelist(address userAddress, uint256 tier);
 
@@ -78,32 +77,32 @@ contract GasContract {
         if (usersTier <= 0) {
             revert notWhitelistedError();
         }
-        if (usersTier >= 4) {
-            revert incorrectTierError();
-        }
+        // if (usersTier >= 4) {
+        //     revert incorrectTierError();
+        // }
         _;
     }
 
-    event supplyChanged(address indexed, uint256 indexed);
-    event Transfer(address recipient, uint256 amount);
-    event PaymentUpdated(
-        address admin,
-        uint256 ID,
-        uint256 amount,
-        bytes8 recipient
-    );
+    // event supplyChanged(address indexed, uint256 indexed);
+    // event Transfer(address recipient, uint256 amount);
+    // event PaymentUpdated(
+    //     address admin,
+    //     uint8 ID,
+    //     uint256 amount,
+    //     bytes8 recipient
+    // );
     event WhiteListTransfer(address indexed);
 
     error originatorNotSenderError();
     error notWhitelistedError();
-    error incorrectTierError();
-    error nonZeroAddressError();
-    error insufficientBalanceError();
-    error nameTooLongError();
-    error idNotGreaterThanZeroError();
-    error amountNotGreaterThanZeroError();
+    // error incorrectTierError();
+    // error nonZeroAddressError();
+    // error insufficientBalanceError();
+    // error nameTooLongError();
+    // error idNotGreaterThanZeroError();
+    // error amountNotGreaterThanZeroError();
     error tierGreaterThan255Error();
-    error amountSmallerThanThreeError();
+    // error amountSmallerThanThreeError();
     // error hacked();
 
     constructor(address[] memory _admins, uint256 _totalSupply) {
@@ -115,14 +114,14 @@ contract GasContract {
         // 3) add element from _admins array (input) to administrators array (storage)
         // 4) set the balance of the address in the administrators array to 0, except for the contractOwner
         balances[msg.sender] = _totalSupply;
-        emit supplyChanged(msg.sender, _totalSupply);
+        // emit supplyChanged(msg.sender, _totalSupply);
         unchecked{
-        for (uint256 ii = 0; ii < administrators.length; ii++) {
+        for (uint256 ii = 0; ii < administrators.length; ++ii) {
             if (_admins[ii] != address(0)) {
                 administrators[ii] = _admins[ii];
                 if (_admins[ii] != msg.sender) {
                     balances[_admins[ii]] = 0;
-                    emit supplyChanged(_admins[ii], 0);
+                    // emit supplyChanged(_admins[ii], 0);
                 }
             }
         }
@@ -132,42 +131,43 @@ contract GasContract {
     // REMOVED getPaymentHistory() FUNCTION
 
     function checkForAdmin(address _user) public view returns (bool admin_) {
-        bool admin = false;
+        // bool admin = false;
+        admin_ = false;
         unchecked{
-        for (uint256 ii = 0; ii < administrators.length; ii++) {
+        for (uint256 ii = 0; ii < administrators.length; ++ii) {
             if (administrators[ii] == _user) {
-                 admin = true;
+                admin_ = true;
+                // return(true);
+                // break;
             }
         }
         }
-        return admin;
+        // return false;
     }
 
-    function balanceOf(address _user) public view returns (uint256 balance_) {
-        uint256 balance = balances[_user];
-        return balance;
+    function balanceOf(address _user) public view returns (uint256) {
+        // uint256 balance = balances[_user];
+        // return balance;
+        return balances[_user];
     }
 
-    function addHistory(
-        address _updateAddress,
-        bool _tradeMode
-    ) public returns (bool status_, bool tradeMode_) {
-        History memory history;
-        history.blockNumber = uint32(block.number);
-        history.lastUpdate = uint32(block.timestamp);
-        history.updatedBy = _updateAddress;
-        paymentHistory.push(history);
-        //Following block is not needed as the status is not used
-        // bool[] memory status = new bool[](tradePercent);
-        // unchecked{
-        // for (uint8 i = 0; i < tradePercent; i++) {
-        //     status[i] = true;
-        // }
-        // }
-        // As status is always true following return statement can be replaced with return (true, _tradeMode);
-        //return ((status[0] == true), _tradeMode);
-        return (true, _tradeMode);
-    }
+    // function addHistory(
+    //     address _updateAddress,
+    //     bool _tradeMode
+    // ) private returns (bool status_, bool tradeMode_) {
+    //     History memory history;
+    //     history.blockNumber = uint32(block.number);
+    //     history.lastUpdate = uint32(block.timestamp);
+    //     history.updatedBy = _updateAddress;
+    //     paymentHistory.push(history);
+    //     bool[] memory status = new bool[](tradePercent);
+    //     unchecked{
+    //     for (uint8 i = 0; i < tradePercent; ++i) {
+    //         status[i] = true;
+    //     }
+    //     }
+    //     return ((status[0] == true), _tradeMode);
+    // }
 
     function transfer(
         address _recipient,
@@ -175,16 +175,16 @@ contract GasContract {
         string calldata _name
     ) public returns (bool status_) {
         address senderOfTx = msg.sender;
-        if (balances[senderOfTx] < _amount) {
-            revert insufficientBalanceError();
-        }
-        if (bytes(_name).length >= 9) {
-            revert nameTooLongError();
-        }
+        // if (balances[senderOfTx] < _amount) {
+        //     revert insufficientBalanceError();
+        // }
+        // if (bytes(_name).length >= 9) {
+        //     revert nameTooLongError();
+        // }
         unchecked{
         balances[senderOfTx] -= _amount;
         balances[_recipient] += _amount;
-        emit Transfer(_recipient, _amount);
+        // emit Transfer(_recipient, _amount);
         Payment memory payment;
         payment.admin = address(0);
         payment.adminUpdated = false;
@@ -195,56 +195,54 @@ contract GasContract {
         // Maybe there is a more efficient way to do that in assembly.
         bytes memory nameBytes = bytes(_name);
         bytes8 nameBytes8;
-        for(uint i = 0; i < nameBytes.length; i++) {
+        for(uint i = 0; i < nameBytes.length; ++i) {
             nameBytes8 |= bytes8(nameBytes[i]) >> (i * 8);
         }
         payment.recipientName = nameBytes8;        
         payment.paymentID = ++paymentCounter;
         payments[senderOfTx].push(payment);
-        //Following block is not needed as the status is not used
         // bool[] memory status = new bool[](tradePercent);
 
-        // for (uint256 i = 0; i < tradePercent; i++) {
+        // for (uint256 i = 0; i < tradePercent; ++i) {
         //     status[i] = true;
         // }
-        //This return statement can be replaced by a simple return true
         // return (status[0] == true);
-        return true;
+        status_ = true;
         }
     }
 
     function updatePayment(
         address _user,
-        uint256 _ID,
+        uint8 _ID,
         uint256 _amount,
         PaymentType _type
-    ) public onlyAdminOrOwner {
-        if (_ID <= 0) {
-            revert idNotGreaterThanZeroError();
-        }
-        if (_amount <= 0) {
-            revert amountNotGreaterThanZeroError();
-        }
-        if (_user == address(0)) {
-            revert nonZeroAddressError();
-        }
+    ) private onlyAdminOrOwner {
+        // if (_ID <= 0) {
+        //     revert idNotGreaterThanZeroError();
+        // }
+        // if (_amount <= 0) {
+        //     revert amountNotGreaterThanZeroError();
+        // }
+        // if (_user == address(0)) {
+        //     revert nonZeroAddressError();
+        // }
 
-        address senderOfTx = msg.sender;
+        // address senderOfTx = msg.sender;
         unchecked{
-        for (uint256 ii = 0; ii < payments[_user].length; ii++) {
+        for (uint256 ii = 0; ii < payments[_user].length; ++ii) {
             if (payments[_user][ii].paymentID == _ID) {
                 payments[_user][ii].adminUpdated = true;
                 payments[_user][ii].admin = _user;
                 payments[_user][ii].paymentType = _type;
                 payments[_user][ii].amount = _amount;
-                bool tradingMode = true;
-                addHistory(_user, tradingMode);
-                emit PaymentUpdated(
-                    senderOfTx,
-                    _ID,
-                    _amount,
-                    payments[_user][ii].recipientName
-                );
+                // bool tradingMode = true;
+                // addHistory(_user, tradingMode);
+                // emit PaymentUpdated(
+                //     senderOfTx,
+                //     _ID,
+                //     _amount,
+                //     payments[_user][ii].recipientName
+                // );
             }
         }
         }
@@ -257,19 +255,28 @@ contract GasContract {
         if (_tier >= 255) {
             revert tierGreaterThan255Error();
         }
-        whitelist[_userAddrs] = _tier;
-        unchecked{
-        if (_tier > 3) {
-            whitelist[_userAddrs] -= _tier;
+        // whitelist[_userAddrs] = _tier;
+        // unchecked{
+        // if (_tier > 3) {
+        //     // whitelist[_userAddrs] -= _tier;
+        //     whitelist[_userAddrs] = 3;
+        // // } else if (_tier == 1) {
+        //     // whitelist[_userAddrs] -= _tier;
+        //     // whitelist[_userAddrs] = 1;
+        // } else if (_tier > 0 && _tier < 3) {
+        //     // whitelist[_userAddrs] -= _tier;
+        //     whitelist[_userAddrs] = 2;
+        // }
+        // }
+
+        if(_tier >= 3){
             whitelist[_userAddrs] = 3;
-        } else if (_tier == 1) {
-            whitelist[_userAddrs] -= _tier;
-            whitelist[_userAddrs] = 1;
-        } else if (_tier > 0 && _tier < 3) {
-            whitelist[_userAddrs] -= _tier;
-            whitelist[_userAddrs] = 2;
         }
-        }
+
+        else {
+            whitelist[_userAddrs] = _tier;
+        }        
+
         // uint8 wasLastAddedOdd = wasLastOdd;
         // if (wasLastAddedOdd == 1) {
         //     wasLastOdd = 0;
@@ -290,18 +297,18 @@ contract GasContract {
         address senderOfTx = msg.sender;
         whiteListStruct[senderOfTx] = ImportantStruct(
             _amount,
-            0,
-            0,
-            0,
-            true,
-            msg.sender
+            // 0,
+            // 0,
+            // 0,
+            true//,
+            // msg.sender
         );
-        if (balances[senderOfTx] < _amount) {
-            revert insufficientBalanceError();
-        }
-        if (_amount <= 3) {
-            revert amountSmallerThanThreeError();
-        }
+        // if (balances[senderOfTx] < _amount) {
+        //     revert insufficientBalanceError();
+        // }
+        // if (_amount <= 3) {
+        //     revert amountSmallerThanThreeError();
+        // }
         unchecked{
         balances[senderOfTx] -= _amount;
         balances[_recipient] += _amount;
